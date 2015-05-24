2ee32ebfd4064fb87209d7f367241c85b0142e24
Ioannis Canellos <iocanel@gmail.com>
2015-05-24 11:07:22 +0300

    Make the image more usable by defaulting to standalone. Make the number of servers configurable (still not dynamic).

--- config-and-run.sh
+++ config-and-run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Persists the ID of the current instance of Zookeeper
-echo ${SERVER_ID} > /opt/zookeeper/data/myid
+
+if [ -z "SERVER_ID" ] [ -z "MAX_SERVERS" ]; then
+  echo "Starting up in clustered mode"
+  for i in 1...$MAX_SERVERS;do
+    echo "server.$i=zookeeper-$i:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
+  done
+
+  # Persists the ID of the current instance of Zookeeper
+  echo ${SERVER_ID} > /opt/zookeeper/data/myid
+  else
+	  echo "Starting up in standalone mode"
+fi
 
 exec /opt/zookeeper/bin/zkServer.sh start-foreground
