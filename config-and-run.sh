78fae7b18d7e3c62320f2fbc23bef1d9b82fff68
Ioannis Canellos <iocanel@gmail.com>
2015-05-24 14:00:49 +0300

    Fix errors in config-and-run.sh

--- config-and-run.sh
+++ config-and-run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-if [ -z "SERVER_ID" ] && [ -z "MAX_SERVERS" ]; then
+echo "$SERVER_ID / $MAX_SERVERS" 
+if [ ! -z "$SERVER_ID" ] && [ ! -z "$MAX_SERVERS" ]; then
   echo "Starting up in clustered mode"
-  for i in 1...$MAX_SERVERS;do
+  echo "" >> /opt/zookeeper/conf/zoo.cfg
+  echo "#Server List" >> /opt/zookeeper/conf/zoo.cfg
+  for i in $( eval echo {1..$MAX_SERVERS});do
     echo "server.$i=zookeeper-$i:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
   done
+  cat /opt/zookeeper/conf/zoo.cfg
 
   # Persists the ID of the current instance of Zookeeper
   echo ${SERVER_ID} > /opt/zookeeper/data/myid
